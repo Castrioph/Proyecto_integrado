661bf346a993dd853c3d4eca6720b53535753834
MSI\Gabriel <gabrieldt26@gmail.com>
2018-05-16 11:50:13 +0200

    subiendo página principal css y html

---
+++ PI_FrontEnd/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+	$('.flexslider').flexslider({
+		prevText: "",
+		nextText: "",
+		pauseOnAction: false,
+		slideshowSpeed: 3000,
+		touch: true
+	});
+    
+});
+
+/*
+$(document).ready(function(){
+	$(".flexslider").hover(function(){
+		$(".flexslider").css({slideshowSpeed: 0});
+		}, function(){
+			$(".flexslider").css({slideshowSpeed: 3000});
+		});
+});
+*/
