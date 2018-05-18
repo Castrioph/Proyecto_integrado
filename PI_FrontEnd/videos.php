b492235f012ac09e8b7eedbaf3940da6b00427f5
ALUMNADO\sacoza <esendier.23@gmail.com>
2018-05-18 10:46:50 +0200

    Sandro modificando

---
+++ PI_FrontEnd/videos.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>LISTADO</title>
+  </head>
+  <body>
+    <style media="screen">
+a{text-decoration: none;}
+.button{background-color:#00CED1;}
+.button:hover{background-color:#F08080;}
+
+    </style>
+  <button class="button"><a href="index.html">Indice</a></button>
+  <br><br>
+    <?php
+include '../src/model/consultas.php';
+$Titulo=$_POST['Titulo'];
+
+$consulta = new consultas();
+$resultado=$consulta->select($Titulo);
+foreach ($resultado as $fila)
+{
+  echo "Título: ".$fila["Titulo"]."<br>";
+  echo "Descripción: ".$fila["Descripcion"]."<br>";
+  echo "Puntuación: ".$fila["Puntuacion"]."<br>";
+  echo "Fecha: ".$fila["Fecha"]."<br>";
+?>
+<form class="" action="insertarComent.php" method="post" onsubmit="return validar()">
+      <input type="text" name="Comentarios" id="Coment" value="Perra Comenta..." onFocus="this.value=''"><br>
+      <input type="hidden" name="id_videos" value="<?= $_GET["id_videos"]?>">
+      <br><br>
+</form>
+<?php
+echo "<a href='videos.php?id=".$fila['Titulo']."'>Comentar</a> <br> <br>";
+}
+ ?>
+  </body>
+</html>
